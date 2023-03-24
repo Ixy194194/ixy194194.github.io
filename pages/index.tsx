3ca99e586a82e3c1222d7f6d739f63e3d1903cf9
Itta Funahashi <itta@itta.dev>
2023-03-24 22:25:55 +0900

    feat: 画像たちを傾ける

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,12 +8,23 @@ export default function Home() {
         <title>いくしー</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="min-h-screen bg-slate-50 space-y-5">
-        <ImageCard src="/dummy.png" />
-        <ImageCard src="/dummy.png" />
-        <ImageCard src="/dummy.png" />
-        <ImageCard src="/dummy.png" />
-        <ImageCard src="/dummy.png" />
+      <div className="min-h-screen bg-slate-50">
+        <div className="absolute flex -rotate-12 gap-5">
+          <div className="flex flex-col gap-6">
+            <ImageCard src="/dummy.png" />
+            <ImageCard src="/dummy.png" />
+            <ImageCard src="/dummy.png" />
+            <ImageCard src="/dummy.png" />
+            <ImageCard src="/dummy.png" />
+          </div>
+          <div className="flex flex-col gap-6">
+            <ImageCard src="/dummy.png" />
+            <ImageCard src="/dummy.png" />
+            <ImageCard src="/dummy.png" />
+            <ImageCard src="/dummy.png" />
+            <ImageCard src="/dummy.png" />
+          </div>
+        </div>
       </div>
     </>
   );
