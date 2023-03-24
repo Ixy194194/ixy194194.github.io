e518d238e3c2848da5b92ccf7b9d545717530a06
Itta Funahashi <itta@itta.dev>
2023-03-25 07:42:06 +0900

    feat: 文字サイズの調整

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,10 @@ export default function Home() {
       </Head>
       <div className="min-h-screen bg-slate-50">
         <div className="absolute flex justify-center items-center w-screen h-screen text-8xl font-bold text-slate-500 z-10">
-          Ｉｘｙ（いくしー）
+          <div>
+            Ｉｘｙ
+            <span className="text-5xl">いくしー</span>
+          </div>
         </div>
 
         <div className="grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] gap-12 z-10 opacity-25">
@@ -30,13 +33,48 @@ export default function Home() {
 
         {/*SNSリンク*/}
         <div className="absolute bottom-0 left-0 w-full h-20 bg-slate-50 z-10">
-            <div className="flex justify-center items-center h-full">
-                <a href="https://twitter.com/Ixy" target="_blank" rel="noopener noreferrer" className="ml-4">Twitter</a>
-                <a href="https://www.instagram.com/ixy__194/?hl=ja" target="_blank" rel="noopener noreferrer" className="ml-4">Instagram</a>
-                <a href="https://www.youtube.com/@ixy" target="_blank" rel="noopener noreferrer" className="ml-4">YouTube</a>
-                <a href="https://ixy.fanbox.cc/" target="_blank" rel="noopener noreferrer" className="ml-4">fanbox</a>
-                <a href="https://ci-en.dlsite.com/creator/5868" target="_blank" rel="noopener noreferrer" className="ml-4">Ci-en</a>
-            </div>
+          <div className="flex justify-center items-center h-full">
+            <a
+              href="https://twitter.com/Ixy"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="ml-4"
+            >
+              Twitter
+            </a>
+            <a
+              href="https://www.instagram.com/ixy__194/?hl=ja"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="ml-4"
+            >
+              Instagram
+            </a>
+            <a
+              href="https://www.youtube.com/@ixy"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="ml-4"
+            >
+              YouTube
+            </a>
+            <a
+              href="https://ixy.fanbox.cc/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="ml-4"
+            >
+              fanbox
+            </a>
+            <a
+              href="https://ci-en.dlsite.com/creator/5868"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="ml-4"
+            >
+              Ci-en
+            </a>
+          </div>
         </div>
       </div>
     </>
