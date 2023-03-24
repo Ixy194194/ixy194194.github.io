54de61c51fd511ba9ebfdb7a43acb49c2f0d5e18
Itta Funahashi <itta@itta.dev>
2023-03-24 22:50:16 +0900

    feat: いくしーを追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,8 +9,10 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="min-h-screen bg-slate-50">
-        {/* <div className="absolute flex -rotate-12 gap-5"> */}
-        <div className="grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] gap-12">
+        <div className="absolute flex justify-center items-center w-screen h-screen text-8xl font-bold text-slate-500 z-10">
+          Ｉｘｙ（いくしー）
+        </div>
+        <div className="grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] gap-12 z-10">
           <ImageCard src="/dummy.jpg" width={180} height={300} />
           <ImageCard src="/dummy.jpg" width={180} height={500} />
           <ImageCard src="/dummy.jpg" width={180} height={300} />
@@ -23,7 +25,6 @@ export default function Home() {
           <ImageCard src="/dummy.jpg" width={180} height={200} />
           <ImageCard src="/dummy.jpg" width={180} height={200} />
           <ImageCard src="/dummy.jpg" width={180} height={100} />
-          {/* </div> */}
         </div>
       </div>
     </>
