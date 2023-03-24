748297906a17ba49e37ceaadb13d6c615542ffdf
Itta Funahashi <itta@itta.dev>
2023-03-25 07:42:06 +0900

    feat: 文字サイズの調整

--- components/profile/Profile.tsx
+++ components/profile/Profile.tsx
@@ -1,23 +1,24 @@
-import { FC } from "react";
-import Image from "next/image";
+import { FC } from 'react';
+import Image from 'next/image';
 
 const Profile: FC = () => {
-    return (
-        <>
-            <div className="fixed flex flex-wrap flex-col justify-center items-center w-full h-screen text-4xl lg:text-8xl font-bold text-slate-500 z-10">
-                <Image
-                    src="/icon.jpeg"
-                    width={150}
-                    height={150}
-                    className="rounded-full mb-8"
-                    alt="illustration"
-                />
-                <span className="dark:text-gray-300">
-                  Ｉｘｙ（いくしー）
-                </span>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="fixed flex flex-wrap flex-col justify-center items-center w-full h-screen text-4xl lg:text-8xl font-bold text-slate-500 z-10">
+        <Image
+          src="/icon.jpeg"
+          width={150}
+          height={150}
+          className="rounded-full mb-8"
+          alt="illustration"
+        />{' '}
+        <div>
+          Ｉｘｙ
+          <span className="text-5xl">いくしー</span>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Profile;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,17 +2,17 @@ import Head from 'next/head';
 import ImageBoard from '../components/imagecard/ImageBoard';
 import SnsLink from '../components/SnsLinks';
 import Profile from '../components/profile/Profile';
-import CommonMeta from '../components/CommonMeta'
+import CommonMeta from '../components/CommonMeta';
 
 export default function Home() {
-    return (
-        <>
-            <CommonMeta></CommonMeta>
-            <div className="min-h-screen bg-slate-50 dark:bg-zinc-900">
-                <Profile></Profile>
-                <ImageBoard></ImageBoard>
-                <SnsLink></SnsLink>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <CommonMeta></CommonMeta>
+      <div className="min-h-screen bg-slate-50 dark:bg-zinc-900">
+        <Profile></Profile>
+        <ImageBoard></ImageBoard>
+        <SnsLink></SnsLink>
+      </div>
+    </>
+  );
 }
