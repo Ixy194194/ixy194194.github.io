fc640ab08fbb0151b2c09c6c92916ca9d42034f3
Itta Funahashi <itta@itta.dev>
2023-03-24 22:04:49 +0900

    feat: setup next.js with tailwindcss

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+import { Inter } from '@next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>いくしー</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="min-h-screen bg-slate-50">Hello, Wolrd</div>
+    </>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./**/*.{ts,tsx}', './**/*.{ts,tsx}', './**/*.{ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
