b5d8ef100e90a96150922be7349b40e4f08441c1
Itta Funahashi <itta@itta.dev>
2023-03-24 22:11:25 +0900

    feat: create ImageCard component

---
+++ components/ImageCard.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+
+interface ImageCardProps {
+  src: string;
+}
+
+const ImageCard: FC<ImageCardProps> = ({ image }) => {
+  return <div className="bg-white"></div>;
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,13 @@ export default function Home() {
         <title>いくしー</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="min-h-screen bg-slate-50">Hello, Wolrd</div>
+      <div className="min-h-screen bg-slate-50">
+        <ImageCard />
+        <ImageCard />
+        <ImageCard />
+        <ImageCard />
+        <ImageCard />
+      </div>
     </>
   );
 }
