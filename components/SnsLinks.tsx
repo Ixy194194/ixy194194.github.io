9f6a3f09214f0d2af9608e78f077ba2d618ec083
Itta Funahashi <itta@itta.dev>
2023-03-27 08:49:23 +0900

    feat: ホバー時の色を白の半透明に

--- components/SnsLinks.tsx
+++ components/SnsLinks.tsx
@@ -18,7 +18,7 @@ const SnsLink: FC = () => {
             href={snslink.url}
             target="_blank"
             rel="noopener noreferrer"
-            className="dark:text-gray-100 px-2 py-1 rounded-md duration-100 ease-out hover:bg-gray-300 dark:hover:bg-zinc-700 active:scale-95"
+            className="dark:text-gray-100 px-2 py-1 rounded-md duration-100 ease-out hover:bg-white/10 active:scale-95"
           >
             {snslink.label}
           </a>
