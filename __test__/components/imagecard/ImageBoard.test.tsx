f9c5eda7d630ec23e1c9f3b4102d88624d8edb0b
ysknsid25 <kengo071225@gmail.com>
2023-03-26 11:54:30 +0900

    chore: 画像表示のテストコードの追加
    Signed-off-by: ysknsid25 <kengo071225@gmail.com>

---
+++ __test__/components/imagecard/ImageBoard.test.tsx
@@ -0,0 +1,11 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import ImageBoard from "../../../components/imagecard/ImageBoard";
+
+describe("ImageBoard", () => {
+    test("12枚の画像が表示されているかどうか", () => {
+        render(<ImageBoard />);
+        const images = screen.getAllByRole("img");
+        expect(images.length).toBe(12);
+    })
+});
