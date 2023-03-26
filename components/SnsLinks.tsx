11f27929095400d2d5e717ab1a04c54974d78893
irhdevel <irihi.design@gmail.com>
2023-03-26 16:29:41 +0900

    ダークモード対応,SNSリンクホバー/クリック時の効果を追加

--- components/SnsLinks.tsx
+++ components/SnsLinks.tsx
@@ -10,10 +10,10 @@ const SnsLink: FC = () => {
         {url: "https://ci-en.dlsite.com/creator/5868", label: "Ci-en"},
     ]
     return (
-        <div className="fixed bottom-0 left-0 w-full h-20 bg-slate-50 z-10">
+        <div className="fixed bottom-0 left-0 w-full h-20 bg-slate-200 z-10 dark:bg-zinc-800">
             <div className="flex justify-center items-center h-full">
                 {snsLinks.map((snslink) => <a key={snslink.url} href={snslink.url} target="_blank"
-                                              rel="noopener noreferrer" className="ml-4">{snslink.label}</a>)}
+                                              rel="noopener noreferrer" className="ml-4 dark:text-gray-100 px-3 py-1 rounded-md duration-100 ease-out hover:bg-gray-300 dark:hover:bg-zinc-700 active:scale-95">{snslink.label}</a>)}
             </div>
         </div>
     );
