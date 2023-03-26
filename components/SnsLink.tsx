ad68aaa088c1b351b672e82fc5ada4074d345104
Itta Funahashi <itta@itta.dev>
2023-03-27 07:53:57 +0900

    feat: remove `ml-4`

--- components/SnsLink.tsx
+++ components/SnsLink.tsx
@@ -7,7 +7,7 @@ interface SnsLinkProps {
 
 const SnsLink: FC<SnsLinkProps> = ({ href, children }) => {
   return (
-    <a href={href} target="_blank" rel="noopener noreferrer" className="ml-4">
+    <a href={href} target="_blank" rel="noopener noreferrer">
       {children}
     </a>
   );
