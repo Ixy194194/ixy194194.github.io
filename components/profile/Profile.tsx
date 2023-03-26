3e643054781460a58e9a6f819c3727034ce43ecc
ysknsid25 <kengo071225@gmail.com>
2023-03-26 11:35:38 +0900

    fix: インデントを修正
    Signed-off-by: ysknsid25 <kengo071225@gmail.com>

--- components/profile/Profile.tsx
+++ components/profile/Profile.tsx
@@ -1,22 +1,20 @@
-import {FC} from 'react';
-import Image from 'next/image';
+import { FC } from "react";
+import Image from "next/image";
 
 const Profile: FC = () => {
     return (
         <>
-            <div
-            className="fixed flex flex-wrap justify-center items-center w-full h-screen text-4xl lg:text-8xl font-bold text-slate-500 z-10">
-                                Ｉｘｙ（いくしー）
-                </div>
-                <div
-            className="fixed flex flex-wrap justify-center items-center w-full h-screen text-4xl lg:text-8xl font-bold text-slate-500 z-10">
+            <div className="fixed flex flex-wrap justify-center items-center w-full h-screen text-4xl lg:text-8xl font-bold text-slate-500 z-10">
+                Ｉｘｙ（いくしー）
+            </div>
+            <div className="fixed flex flex-wrap justify-center items-center w-full h-screen text-4xl lg:text-8xl font-bold text-slate-500 z-10">
                 <Image
-                            src="/icon.jpeg"
-                            width={150}
-                            height={150}
-                            className="mb-80 rounded-full"
-                            alt="illustration"
-                        />
+                    src="/icon.jpeg"
+                    width={150}
+                    height={150}
+                    className="mb-80 rounded-full"
+                    alt="illustration"
+                />
             </div>
         </>
     );
