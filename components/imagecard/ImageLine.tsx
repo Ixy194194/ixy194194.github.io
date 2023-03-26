92bcef67e775367b781572416ba5c4ab4c28f2c7
Fairy_Phy <phy.public@gmail.com>
2023-03-26 13:08:30 +0900

    インデントのTabをスペースに変換

--- components/imagecard/ImageLine.tsx
+++ components/imagecard/ImageLine.tsx
@@ -2,15 +2,15 @@ import { FC } from "react";
 import { ImageLineProps } from '../../types/ImageLineProps';
 
 const ImageLine: FC<ImageLineProps> = ({ lineArray }) => {
-	return (
-		<div className="absolute flex gap-12 opacity-25 px-6 py-8 image-line-anim">
+    return (
+        <div className="absolute flex gap-12 opacity-25 px-6 py-8 image-line-anim">
             {lineArray.map((v, i) => (
                 <div key={i} className="flex justify-between gap-12">
                     {v}
                 </div>
             ))}
-    	</div>
-	);
+        </div>
+    );
 };
 
 export default ImageLine;
