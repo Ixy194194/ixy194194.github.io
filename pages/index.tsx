65dc31f1785731f136474738c7f4bda485d05169
Itta Funahashi <itta@itta.dev>
2023-03-26 17:46:24 +0900

    Revert "feat: ダミー画像を増やした"
    This reverts commit f9180671840ed36e6a9818f41cf62b79bfe09ebe.

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,18 +15,6 @@ const images = [
   '/010.jpg',
   '/011.jpg',
   '/012.jpg',
-  '/001.jpg',
-  '/002.jpg',
-  '/003.jpg',
-  '/004.jpg',
-  '/005.jpg',
-  '/006.jpg',
-  '/007.jpg',
-  '/008.jpg',
-  '/009.jpg',
-  '/010.jpg',
-  '/011.jpg',
-  '/012.jpg',
 ];
 
 export default function Home() {
