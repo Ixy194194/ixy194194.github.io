c49a4c685978c8e0f62fe619736a79f92a6333c3
Itta Funahashi <itta@itta.dev>
2023-03-27 09:00:46 +0900

    feat: テストを修正

--- __test__/components/profile/Profile.test.tsx
+++ __test__/components/profile/Profile.test.tsx
@@ -1,11 +1,11 @@
-import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
-import Profile from "../../../components/profile/Profile";
+import '@testing-library/jest-dom';
+import { render, screen, within } from '@testing-library/react';
+import Profile from '../../../components/profile/Profile';
 
-describe("Profile", () => {
-    test("Profile名が表示されているか", () => {
-        render(<Profile />);
-        const text = screen.getByText("Ｉｘｙ（いくしー）");
-        expect(text).toBeInTheDocument();
-    });
+describe('Profile', () => {
+  test('Profile名が表示されているか', () => {
+    render(<Profile />);
+    const { getByText } = within(screen.getByText('Ｉｘｙ'));
+    expect(getByText('いくしー')).toBeInTheDocument();
+  });
 });
