e68da053ab92eaee6b0065263c2d6bb2e085c394
Itta Funahashi <itta@itta.dev>
2023-03-27 11:42:08 +0900

    feat: 重なり順を修正

--- components/profile/Profile.tsx
+++ components/profile/Profile.tsx
@@ -4,14 +4,14 @@ import Image from 'next/image';
 const Profile: FC = () => {
   return (
     <>
-      <div className="fixed flex flex-wrap flex-col justify-center items-center w-full h-screen text-4xl lg:text-8xl font-bold text-slate-500 z-10">
+      <div className="fixed flex flex-wrap flex-col justify-center items-center w-full h-screen text-4xl lg:text-8xl font-bold text-slate-500 z-20">
         <Image
           src="/icon.jpeg"
           width={150}
           height={150}
           className="rounded-full mb-8"
           alt="illustration"
-        />{' '}
+        />
         <div>
           Ｉｘｙ
           <span className="text-5xl">いくしー</span>
