c6010e9b8b3faa5b4b3907073fc60d763b6f7899
Itta Funahashi <itta@itta.dev>
2023-03-27 11:54:02 +0900

    feat: リンクが押せなくなってしまったのを修正

--- components/SnsLinks.tsx
+++ components/SnsLinks.tsx
@@ -10,7 +10,7 @@ const SnsLink: FC = () => {
     { url: 'https://ci-en.dlsite.com/creator/5868', label: 'Ci-en' },
   ];
   return (
-    <div className="absolute bottom-0 left-0 w-full h-20 bg-white/10 dark:bg-black/10 backdrop-blur-md shadow-[inset_0_32px_32px_-32px_rgba(255,255,255,0.4)] z-10">
+    <div className="absolute bottom-0 left-0 w-full h-20 bg-white/10 dark:bg-black/10 backdrop-blur-md shadow-[inset_0_32px_32px_-32px_rgba(255,255,255,0.4)] z-30">
       <div className="flex justify-center items-center h-full">
         {snsLinks.map((snslink) => (
           <a
