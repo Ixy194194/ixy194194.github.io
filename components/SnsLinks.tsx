652b90ed67254a125233ff115854aea3e4fc6f1a
Fairy_Phy <phy.public@gmail.com>
2023-03-28 05:30:24 +0900

    何故か消されているダークモードの復活

--- components/SnsLinks.tsx
+++ components/SnsLinks.tsx
@@ -3,7 +3,7 @@ import SnsLink from './SnsLink';
 
 const SnsLinks: FC = () => {
     return (
-        <div className="fixed bottom-0 left-0 w-full h-20 bg-slate-50 z-10">
+        <div className="fixed bottom-0 left-0 w-full h-20 bg-slate-200 z-10 dark:bg-zinc-800">
             <div className="flex justify-center items-center h-full space-x-4">
                 <SnsLink href="https://twitter.com/Ixy">Twitter</SnsLink>,
                 <SnsLink href="https://www.instagram.com/ixy__194/?hl=ja">Instagram</SnsLink>,
