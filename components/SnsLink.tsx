f64a99a0c95cae698a3d9cd875bd29711be58b12
Itta Funahashi <itta@itta.dev>
2023-03-27 11:57:18 +0900

    feat: ライトモードでのホバー時の背景色を修正

--- components/SnsLink.tsx
+++ components/SnsLink.tsx
@@ -11,7 +11,7 @@ const SnsLink: FC<SnsLinkProps> = ({ href, children }) => {
 			href={href}
 			target="_blank"
 			rel="noopener noreferrer"
-			className="dark:text-gray-100 px-2 py-1 rounded-md duration-100 ease-out hover:bg-white/10 active:scale-95"
+			className="dark:text-gray-100 px-2 py-1 rounded-md duration-100 ease-out hover:bg-slate-400/10 dark:hover:bg-white/10 active:scale-95"
 		>
 			{children}
 		</a>
