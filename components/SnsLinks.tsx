c8de34436371d9cf2ff431948c9c776e7d11fe42
Itta Funahashi <itta@itta.dev>
2023-03-27 12:16:46 +0900

    feat: 縁の白色を強くした

--- components/SnsLinks.tsx
+++ components/SnsLinks.tsx
@@ -3,7 +3,7 @@ import SnsLink from './SnsLink';
 
 const SnsLinks: FC = () => {
 	return (
-		<div className="absolute bottom-0 left-0 w-full h-20 bg-white/10 dark:bg-black/10 backdrop-blur-md shadow-[inset_0_32px_32px_-32px_rgba(255,255,255,0.4)] z-30">
+		<div className="absolute bottom-0 left-0 w-full h-20 bg-white/10 dark:bg-black/10 backdrop-blur-md shadow-[inset_0_32px_32px_-32px_rgba(255,255,255,0.8)] z-30">
 			<div className="flex justify-center items-center h-full space-x-4">
 				<SnsLink href="https://twitter.com/Ixy">Twitter</SnsLink>,
 				<SnsLink href="https://www.instagram.com/ixy__194/?hl=ja">Instagram</SnsLink>,
