51bb13965fb0e0aaf86836cabe3e180da422cd8b
irhdevel <irihi.design@gmail.com>
2023-03-28 22:39:42 +0900

    画面の大きい端末では感覚を大きく取るように変更

--- components/SnsLinks.tsx
+++ components/SnsLinks.tsx
@@ -4,7 +4,7 @@ import SnsLink from './SnsLink';
 const SnsLinks: FC = () => {
 	return (
 		<div className="fixed bottom-0 left-0 w-full h-20 bg-slate-200 z-10 dark:bg-zinc-800">
-			<div className="flex flex-wrap justify-center items-center h-full dark:text-gray-100">
+			<div className="flex flex-wrap justify-center items-center h-full dark:text-gray-100 sm:space-x-4">
 				<SnsLink href="https://twitter.com/Ixy">Twitter</SnsLink>,
 				<SnsLink href="https://www.instagram.com/ixy__194/?hl=ja">Instagram</SnsLink>,
 				<SnsLink href="https://www.youtube.com/@ixy">YouTube</SnsLink>,
