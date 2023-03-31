9d2a5dc86c35f5716806ba39cc07ef4dc5760b70
Itta Funahashi <itta@itta.dev>
2023-03-31 10:05:34 +0900

    文字サイズを変更

--- components/profile/Profile.tsx
+++ components/profile/Profile.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 const Profile: FC = () => {
 	return (
 		<>
-			<div className="fixed flex flex-wrap flex-col justify-center items-center w-full h-screen text-4xl lg:text-8xl font-bold text-slate-500 z-20">
+			<div className="fixed flex flex-wrap flex-col justify-center items-center w-full h-screen text-5xl lg:text-7xl font-bold text-slate-500 z-20">
 				<Image
 					src="/icon.jpeg"
 					width={150}
