8313191a3e044907c998bc6864d4cacef40c9465
irhdevel <irihi.design@gmail.com>
2023-05-21 19:12:00 +0900

    ライトモード時の上部のボーダーをよりはっきり見えるようにした

--- components/SnsLinks.tsx
+++ components/SnsLinks.tsx
@@ -7,7 +7,7 @@ interface SnsLinksProps {
 }
 const SnsLinks: FC<SnsLinksProps> = ({setmodalstate}) => {
 	return (
-		<div className="absolute bottom-0 left-0 w-full h-20 bg-white/10 dark:bg-black/10 backdrop-blur-md border-t border-t-white/10 z-30">
+		<div className="absolute bottom-0 left-0 w-full h-20 bg-white/10 dark:bg-black/10 backdrop-blur-md border-t border-slate-400/30 z-30">
 			<div className="flex flex-wrap justify-center items-center h-full space-x-4">
 				<SnsLink href="https://twitter.com/Ixy">Twitter</SnsLink>
 				<SnsLink href="https://www.instagram.com/ixy__194/?hl=ja">Instagram</SnsLink>
